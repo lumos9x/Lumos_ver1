5ec99c4c7fe8d31c3154cd583c87886eb4514b29
arara90 <ararajo@gmail.com>
2019-08-09 01:07:01 +0900

    Upgrade Crawler & Match column names of result dataframes

--- Collecting/Roading.py
+++ Collecting/Roading.py
@@ -4,7 +4,7 @@ import os
 
 class Roading:
     def preprocessing(self, df, category_nm):
-        # 공통 전처리 부분 / df 넘기기 전에 df 컬럼 [명칭, 위도, 경도] 로 통일할 것.
+        # 공통 전처리 부분 / df 넘기기 전에 df 컬럼 [명칭,주소, 위도, 경도] 로 통일할 것.
         df1 = df.dropna()                               # 1. NaN 값이 하나라도 들어가 있는 행은 그 행 전체를 제거
         df1['분류'] = category_nm                        # 2. 분류 추가하기.
         df2 = df1.drop_duplicates(keep='first')         # 3. 중복되는 행 하나만 남기고 제거하기(부정확한 데이터)
@@ -29,6 +29,7 @@ class Roading:
         # dong_df[dong_df.위도.isnull()]
         dong_df2 = pd.DataFrame()
         dong_df2['명칭'] = dong_df['보안등위치명']
+        dong_df2['주소'] = dong_df['소재지지번주소']
         dong_df2['위도'] = dong_df['위도']
         dong_df2['경도'] = dong_df['경도']
         return self.preprocessing(dong_df2,'보안등')
@@ -39,6 +40,7 @@ class Roading:
         dongcc_df = gucc_df[gucc_df.소재지지번주소.str.contains(dong)]
         dong_df2 = pd.DataFrame()
         dong_df2['명칭'] = dongcc_df['소재지지번주소']
+        dong_df2['주소'] = dongcc_df['소재지지번주소']
         dong_df2['위도'] = dongcc_df['위도']
         dong_df2['경도'] = dongcc_df['경도']
 
